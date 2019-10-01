7a7879c4f76c295a20e333af1702331adb1d9238
alenavee <alenavarg@mail.ru>
2019-10-01 23:26:38 +0300

    vigenere finished

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,4 +1,4 @@
-def encrypt_vigenere(plaintext, keyword):
+def encrypt_vigenere(plaintext, keyword) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
     >>> encrypt_vigenere("PYTHON", "A")
@@ -8,11 +8,23 @@ def encrypt_vigenere(plaintext, keyword):
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    # PUT YOUR CODE HERE
+    ciphertext = ''
+    for i, ch in enumerate(plaintext):
+        key_ch = keyword[i % len(keyword)]
+        if "A" <= key_ch <= "Z":
+            shift = ord(key_ch) - ord('A')
+        else:
+            shift = ord(key_ch) - ord('a')
+
+        if "A" <= ch <= "Z":
+            ciphertext += chr(ord("A") + (ord(ch) - ord("A") + shift) % 26)
+        else:
+            ciphertext += chr(ord("a") + (ord(ch) - ord("a") + shift) % 26)
+
     return ciphertext
 
 
-def decrypt_vigenere(ciphertext, keyword):
+def decrypt_vigenere(ciphertext, keyword) -> str:
     """
     Decrypts a ciphertext using a Vigenere cipher.
     >>> decrypt_vigenere("PYTHON", "A")
@@ -22,5 +34,17 @@ def decrypt_vigenere(ciphertext, keyword):
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
-    # PUT YOUR CODE HERE
+    plaintext = ''
+    for i, ch in enumerate(ciphertext):
+        key_ch = keyword[i % len(keyword)]
+        if 'A' <= key_ch <= 'Z':
+            shift = ord(key_ch) - ord('A')
+        else:
+            shift = ord(key_ch) - ord('a')
+
+        if 'A' <= ch <= 'Z':
+            plaintext += chr(ord('A') + (26 + ord(ch) - ord('A') - shift) % 26)
+        else:
+            plaintext += chr(ord("a") + (26 + ord(ch) - ord("a") - shift) % 26)
+
     return plaintext
